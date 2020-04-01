e305d0cba02445b1acdf57f796490c920b51a9a9
Brian Njau <brian.njau@strathmore.edu>
2020-04-01 10:06:55 +0300

    Updates

---
+++ precache-manifest.97679844d963f812043fcf04d167bfa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fe1b779920c7b1b89fa486113fe1d8",
+    "url": "/monsters-searcher/index.html"
+  },
+  {
+    "revision": "fa82c666de80ae7b219c",
+    "url": "/monsters-searcher/static/css/main.2bbfeab3.chunk.css"
+  },
+  {
+    "revision": "6901eb4e43198a160a3d",
+    "url": "/monsters-searcher/static/js/2.a05dcfec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-searcher/static/js/2.a05dcfec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa82c666de80ae7b219c",
+    "url": "/monsters-searcher/static/js/main.98bac913.chunk.js"
+  },
+  {
+    "revision": "fef3049a4e1c38cc10c3",
+    "url": "/monsters-searcher/static/js/runtime-main.35584c30.js"
+  }
+]);
