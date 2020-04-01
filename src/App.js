25c7266ea4233fc0a84203a881dc032d6fa98845
Brian Njau <brian.njau@strathmore.edu>
2020-04-01 08:46:41 +0300

    Completed Project

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+import { SearchBox } from "./components/search-box/search-box.component";
+import { CardList } from './components/card-list/card-list.component';
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+          monsters:[],
+          searchField:''
+        };
+        
+
+    }
+  
+
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(Response=> Response.json())
+      .then(users =>this.setState({monsters:users}));
+
+
+
+    }
+    handleChange = (e) => {
+
+      this.setState({searchField:e.target.value})
+
+    }
+
+  render(){
+    const { monsters,searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+  return (
+    <div className="App">
+    <h1>Monsters Searcher</h1>
+        <SearchBox
+      placeholder = 'Search Monsters'
+      handlechange= {this.handleChange}
+
+          />
+
+    <CardList monsters = {filteredMonsters} /> 
+      
+    </div>
+  );
+  }
+}
+
+export default App;
